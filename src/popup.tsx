55c0dd5daa01825cf0ef13ce524821408a8227f5
serh11p <git@serh11p.com>
2020-12-03 16:54:17 +0200

    Add basic sample for persisting in popup mode

--- src/popup.tsx
+++ src/popup.tsx
@@ -3,9 +3,23 @@ import "./main.css";
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import { browser } from "webextension-polyfill-ts";
+import {
+  listen,
+  createLocationState,
+  navigate,
+  HistoryAction,
+} from "lib/woozie";
 import { isPopupModeEnabled } from "lib/popup-mode";
 import { WindowType, openInFullPage } from "app/env";
 import App from "app/App";
+
+const savedPageStr = localStorage.getItem("popup_saved_page");
+if (savedPageStr) {
+  const { pathname, search, hash, savedAt } = JSON.parse(savedPageStr);
+  if (savedAt > Date.now() - 30_000) {
+    navigate({ pathname, search, hash }, HistoryAction.Replace);
+  }
+}
 
 ReactDOM.render(
   <App env={{ windowType: WindowType.Popup }} />,
@@ -17,3 +31,16 @@ if (!popups.includes(window) || !isPopupModeEnabled()) {
   openInFullPage();
   window.close();
 }
+
+listen(() => {
+  const { pathname, search, hash } = createLocationState();
+  localStorage.setItem(
+    "popup_saved_page",
+    JSON.stringify({
+      pathname,
+      search,
+      hash,
+      savedAt: Date.now(),
+    })
+  );
+});
